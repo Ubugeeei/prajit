415d94c720c720b2060cae1cc127c983fbd9203f
Ubugeeei <ubuge1122@gmail.com>
2023-05-09 00:04:56 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,14 @@ mod parse;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() == 0 {
+    if args.is_empty() {
         println!("argument is not provided.");
         std::process::exit(1);
     }
 
-    let is_interactive = match args.iter().find(|it| it == &"-i" || it == &"--interactive") {
-        Some(_) => true,
-        None => false,
-    };
-
-    let is_emit_assembly = match args
-        .iter()
-        .find(|it| it == &"-e" || it == &"--emit-assembly")
-    {
-        Some(_) => true,
-        None => false,
-    };
-
-    let is_jit_compile = match args.iter().find(|it| it == &"-j" || it == &"--jit-compile") {
-        Some(_) => true,
-        None => false,
-    };
+    let is_interactive = args.iter().any(|it| it == "-i" || it == "--interactive");
+    let is_emit_assembly = args.iter().any(|it| it == "-e" || it == "--emit-assembly");
+    let is_jit_compile = args.iter().any(|it| it == "-j" || it == "--jit-compile");
 
     if is_interactive {
         start_repl(is_jit_compile);
